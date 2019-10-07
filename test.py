afbefcedb9e5ffbaf59f0733750658d60724ac14
Maria <manya1999m09@yandex.ru>
2019-10-07 16:05:58 +0300

    test changed with cralwer

--- test.py
+++ test.py
@@ -1,18 +1,18 @@
 import unittest
-import task1
+import crawler
 
 
 class TestProg(unittest.TestCase):
     def test_structure(self):
-        artjson = task1.publish_report('data_file.json', task1.articles)
+        artjson = crawler.publish_report('data_file.json', crawler.find_articles(crawler.get_html_page('https://sobesednik.ru/psychology')))
         self.assertEqual(artjson["url"] == "", False)
 
     def test_structarticle(self):
-        artjson = task1.publish_report('data_file.json', task1.articles)
-        self.assertEqual(artjson["articles"] == {}, False)
+        artjson = crawler.publish_report('data_file.json', crawler.find_articles(crawler.get_html_page('https://sobesednik.ru/psychology')))
+        self.assertEqual(artjson["articles"][0]['title'] == '', False)
 
     def test_get_html_page(self):
-        self.assertEqual(task1.get_html_page("https://sobesednik.ru/psychology") == 0, False)
+        self.assertEqual(crawler.get_html_page("https://sobesednik.ru/psychology") == 0, False)
 
 
 if __name__ == '__main__':
