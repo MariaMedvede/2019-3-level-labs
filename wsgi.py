18775d0b2a249c143cb0cb8acdd5dd6e28a469bd
TimurKM <kakabaevtm@mail.ru>
2019-10-14 18:14:00 +0300

    obnovlenie

--- wsgi.py
+++ wsgi.py
@@ -1,17 +1,25 @@
 import json
-from flask import Flask, render_template
-from jinja2 import Template
+from flask import Flask, render_template, request, redirect, url_for
 import crawler
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def start():
-    crawler.publish_report('data_file.json', crawler.find_articles(crawler.get_html_page('https://sobesednik.ru/psychology')))
+    crawler.publish_report('data_file.json',
+                           crawler.find_articles(crawler.get_html_page('https://sobesednik.ru/psychology')))
     with open("data_file.json", "r") as read_file:
         data = json.load(read_file)
         link = data["url"]
         articles = data["articles"]
     return render_template('template.html', link=link, list=articles)
 
-if __name__ =="__main__":
-    app.run(host = '0.0.0.0', port=8000, debug=True)
+
+@app.route('/process_data/', methods=['POST'])
+def doit():
+    return redirect(url_for("start"))
+
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=5000, debug=True)
