3f8f89eff7f45b343107eb08b0f453694a712026
Maria <manya1999m09@yandex.ru>
2019-10-17 17:08:58 +0300

    error

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,8 @@
 import json
 from flask import Flask, render_template, request, redirect, url_for
 import crawler
+from datetime import datetime, date
+
 
 app = Flask(__name__)
 
@@ -13,10 +15,16 @@ def start():
         data = json.load(read_file)
         link = data["url"]
         articles = data["articles"]
-    return render_template('template.html', link=link, list=articles)
+        return render_template('template.html', link=link, list=articles)
+
+@app.errorhandler(404)
+def not_found_error(error):
+    now = datetime.now()
+    return render_template('error.html', now=now), 404
 
 @app.route('/process_data/', methods=['POST'])
 def doit():
+    print(url_for("start"))
     return redirect(url_for("start"))
 
 
